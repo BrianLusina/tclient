75133238f8d4abb1a60527071b77a725bc1d40b0
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-07-19 09:36:07 +0300

    feat(bitfield): bitfield pieces

---
+++ pkg/bitfield/bitfield.go
@@ -0,0 +1,27 @@
+package bitfield
+
+// Bitfield represents the pieces that a peer has
+type Bitfield []byte
+
+// HasPiece teels if a bitfield has a particular index
+func (bf Bitfield) HasPiece(index int) bool {
+	byteIndex := index / 8
+	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+	return bf[byteIndex]>>(7-offset)&1 != 0
+}
+
+// SetPiece sets a bit in the bitfield
+func (bf Bitfield) SetPiece(index int) {
+	byteIndex := index / 8
+	offset := index % 8
+
+	// silently ignore invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+
+	bf[byteIndex] |= 1 << (7 - offset)
+}
